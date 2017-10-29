d5d0b50e66e427fff64b92e0db5ae610cc6ca017
nj4710 <nj4710@gmail.com>
2017-10-29 13:25:24 +0530

    remove extra comments

--- linked_lists/rotate.cpp
+++ linked_lists/rotate.cpp
@@ -3,15 +3,13 @@ Please note that it's Function problem i.e.
 you need to write your solution in the form Function(s) only.
 Driver Code to call/invoke your function would be added by GfG's Online Judge.*/
 
-
 /*The structure of linked list is the following
 struct Node
 {
 int data;
 Node* next; 
 };*/
-/*The function removes the loop from the linked list if present
-You are required to complete this method*/
+
 void removeTheLoop(Node *node)
 {
      //Your code here
