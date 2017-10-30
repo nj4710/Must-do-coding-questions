e3b12394aa563589be96f6dfc7a1b8f9de3b9d69
nj4710 <nj4710@gmail.com>
2017-10-30 23:59:47 +0530

    extra line

--- linked_lists/stack_using_linked_list.cpp
+++ linked_lists/stack_using_linked_list.cpp
@@ -3,7 +3,6 @@ Please note that it's Function problem i.e.
 you need to write your solution in the form Function(s) only.
 Driver Code to call/invoke your function would be added by GfG's Online Judge.*/
 
-
 /* 
 The structure of the node of the stack is
 struct StackNode
