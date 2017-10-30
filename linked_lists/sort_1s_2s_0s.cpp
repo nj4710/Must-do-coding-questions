6ef682c3a3849dc5a9ae24daafb7510bac5a25dd
nj4710 <nj4710@gmail.com>
2017-10-30 23:58:34 +0530

    removed extra line

--- linked_lists/sort_1s_2s_0s.cpp
+++ linked_lists/sort_1s_2s_0s.cpp
@@ -3,7 +3,6 @@ Please note that it's Function problem i.e.
 you need to write your solution in the form Function(s) only.
 Driver Code to call/invoke your function would be added by GfG's Online Judge.*/
 
-
 /*
   Sort the list of 0's,1's and 2's
   The input list will have at least one element  
